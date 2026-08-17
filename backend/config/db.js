7da6d622d56480ad797903ee2524a99189647a47
akshay971949 <akcartoon1255@gmail.com>
2026-08-18 01:20:44 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,7 +10,10 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  dateStrings: true
+  dateStrings: true,
+  ssl: {
+    rejectUnauthorized: false
+   }
 });
 
 module.exports = pool;
